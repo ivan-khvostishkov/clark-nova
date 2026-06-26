5ac7481a77d63557de78146824762e5c1c72c55b
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2026-06-26 11:45:19 +0200

    v4.0

--- clarknova.nosocial.net/sw.js
+++ clarknova.nosocial.net/sw.js
@@ -1,8 +1,17 @@
-const CACHE_NAME = 'clark-nova-v31';
+const CACHE_NAME = 'clark-nova-v40';
 const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
+  '/audio/COMType_Typewriter%20Single%20Strike%201%20DPA4060_PMSFX_CWT.wav',
+  '/audio/COMType_Typewriter%20Space%20Spacebar%2063%20DPA4060_PMSFX_CWT.wav',
+  '/audio/COMType_Typewriter%20Backspace%2081%20DPA4060_PMSFX_CWT.wav',
+  '/audio/COMType_Typewriter%20Carriage%20Return%20Movement%2092%20DPA4060_PMSFX_CWT.wav',
+  '/audio/COMType_Typewriter%20Bell%20223%20DPA4060_PMSFX_CWT.wav',
+  '/audio/COMType_Typewriter%20Shit%20Press%20Release%2036%20DPA4060_PMSFX_CWT.wav',
+  '/audio/COMType_Typewriter%20Shit%20Press%20Release%2037%20DPA4060_PMSFX_CWT.wav',
+  '/audio/COMType_Typewritter%20Roller%20Knob%20Movement%20208%20DPA4060_PMSFX_CWT.wav',
+  '/audio/COMType_Typewritter%20Lever%20182%20DPA4060_PMSFX_CWT.wav',
   'https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;700&family=Special+Elite&family=EB+Garamond:ital,wght@1,500;1,700&display=swap'
 ];
 
